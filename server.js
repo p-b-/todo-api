e28214e772b56e276da721a7ecbd5cfa4e932bbd
Paul Bentley <paulbentley73@gmail.com>
2016-02-05 23:52:25 +0000

    Testing issue with heroku

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.post('/users', function(req, resp) {
 });
 
 app.post('/users/login', function(req, resp) {
-  // resp.status(200).send();
+  resp.status(200).send();
   // var body = _.pick(req.body, 'email', 'password');
   //
   // db.user.authenticate(body).then(function(user) {
